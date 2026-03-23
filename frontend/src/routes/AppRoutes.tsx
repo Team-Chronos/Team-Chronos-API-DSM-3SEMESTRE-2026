1e9b12437c5fc31bdbdece475a37b4228e7267ee
Aline Raquel Camargo <aline.oliveira105@fatec.sp.gov.br>
2026-03-23 10:27:30 -0300

    fix(#55): correcao estrutura de pastas

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,6 +1,6 @@
 import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
-import CadastroProfissional  from "../pages/CadastroProfissionais";
+import CadastroProfissional  from "../pages/cadastroProfissionail";
 
 const Layout = lazy(() => import("../components/Layout"))
 
